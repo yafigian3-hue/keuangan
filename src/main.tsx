1db6f1929ff45c1a8f3f1e0e4e62296a897bf4e8
yafigian3-hue <yafigian3@gmail.com>
2026-01-01 12:07:05 +0700

    Fix build error: unused StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client"
+import App from "./App"
+import "./index.css"
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <App />
 )
