d6eb67ef7f1ee6477e3813552b8e054162d13d05
yafigian3-hue <yafigian3@gmail.com>
2026-01-02 21:03:58 +0700

    Initial commit - production ready

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
+// src/main.tsx
 import { createRoot } from "react-dom/client"
+import { HashRouter } from "react-router-dom"
 import App from "./App"
 import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
-  <App />
+  <HashRouter>
+    <App />
+  </HashRouter>
 )
