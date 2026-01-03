b60024103d07b589ed3fb2d91bfcb4bdcf3c6037
yafigian3-hue <yafigian3@gmail.com>
2026-01-03 18:29:37 +0700

    Deploy build output for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
-// src/main.tsx
 import { createRoot } from "react-dom/client"
-import { HashRouter } from "react-router-dom"
 import App from "./App"
 import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <App />
 )
